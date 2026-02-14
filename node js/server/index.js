c11584c3f4aef73dff7a5a1f4877a720cc0a32c7
Srishti2006-coder <srishtibansal505@gmail.com>
2026-02-14 22:10:15 +0530

    create servers , req,res and set localhost 3000 with a mesg Server is running

--- node js/server/index.js
+++ node js/server/index.js
@@ -4,8 +4,11 @@ const http = require('http');
 //   is http ka name hum kuch bhi likh skte hai , ('http') is builtin 
 
 const myServer = http.createServer((req, res) => {
-    console.log("New request received");
-    res.end("Hello from Server");
+  //  console.log("New request received");   // this will show in the terminal when we type npm start
+
+  console.log(req.headers);     // headers ki info , client ki and all
+
+    res.end("Hello from Server again ");   // this shows in the browser when we hit localhost:3000
 });
 
 myServer.listen(3000, () => {
