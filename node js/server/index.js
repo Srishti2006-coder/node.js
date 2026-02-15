99fc727d17346601421940edab0f5378de4cd175
Srishti2006-coder <srishtibansal505@gmail.com>
2026-02-15 12:18:47 +0530

    Http methods -> GET, POST, PUT, PATCH, DELETE

--- node js/server/index.js
+++ node js/server/index.js
@@ -23,7 +23,7 @@
 // Log bnaya jiske help se time aa rha hai jb bhi new entries hoyegi 
 // switch case bnaya jisme /, /aboutus and ek defualt case bnaya
 // url = protocol, https , domain and , path
-
+// url = UNIFORM RESOURCE LOCATOR
 
 // const http = require("http");
 // const fs = require("fs");
@@ -54,6 +54,7 @@
 // });
 
 
+
 // req.url
 
 const http = require("http");
@@ -64,10 +65,10 @@ const myServer = http.createServer((req, res) => {
 
     if (req.url === "/favicon.ico") return res.end();
 
-    const log = `${Date.now()}: ${req.url} New Req Received\n`;
-    const myUrl = url.parse(req.url, true);
+    const log = `${Date.now()}: ${req.url} New  Req Received\n`;
+    const myUrl = url.parse(req.url, true);        // 
 
-    console.log(myUrl.pathname);
+    console.log(myUrl.pathname);          // pathname extract 
 
     fs.appendFile("log.txt", log, (err, data) => {
 
@@ -77,8 +78,14 @@ const myServer = http.createServer((req, res) => {
                 break;
 
             case "/about":
-                res.end("I am Piyush Garg");
+                const username = myUrl.query.myname;    // query paramters extracts
+                res.end(`Hi, ${username}`);       
+            //    res.end("I am Piyush Garg");
                 break;
+            
+            case "/search":
+                const search = myUrl.query.search_query;
+                res.end("Here are your result for " + search);
 
             default:
                 res.end("404 Not Found");
@@ -89,3 +96,5 @@ const myServer = http.createServer((req, res) => {
 myServer.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
+
+
